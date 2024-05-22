52afade7ec7851eff915d7552c0eae2486e2f1d9
GlendyT <glendytuyuc@gmail.com>
2024-05-21 20:02:13 -0600

    iniciar a mostrar los detalles de los productos, agregar Loader de react-router-dom

---
+++ src/components/ProductDetails.tsx
@@ -0,0 +1,42 @@
+import { useNavigate } from "react-router-dom";
+import { Product } from "../types";
+import { formatCurrency } from "../utils";
+
+type ProductDetailsProps = {
+  product: Product;
+};
+
+export default function ProductDetails({ product }: ProductDetailsProps) {
+  const navigate = useNavigate();
+
+  const isAvailable = product.availability;
+  return (
+    <tr className="border-b ">
+      <td className="p-3 text-lg text-gray-800">{product.name}</td>
+      <td className="p-3 text-lg text-gray-800">
+        {formatCurrency(product.price)}
+      </td>
+      <td className="p-3 text-lg text-gray-800">
+        {isAvailable ? "Disponible" : "No Disponible"}
+      </td>
+      <td className="p-3 text-lg text-gray-800 ">
+        <div className="flex gap-2 items-center">
+          <button
+            onClick={() => navigate(`/productos/${product.id}/editar`)}
+            className="bg-pink-700 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center"
+          >
+            Editar
+          </button>
+        </div>
+      </td>
+    </tr>
+  );
+}
+
+
+/*          <Link
+            to={`/productos/${product.id}/editar`}
+            className="bg-pink-700 text-white rounded-lg w-full p-2 uppercase font-bold text-xs text-center"
+          >
+            Editar
+          </Link>*/
--- src/router.tsx
+++ src/router.tsx
@@ -1,7 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./layouts/Layout";
-import Products from "./views/Products";
+import Products, {loader as productsLoader} from "./views/Products";
 import NewProduct, { action as newProductAction} from "./views/NewProduct";
+import EditProduct, { loader as editProductLoader } from "./views/EditProduct";
 
 export const router = createBrowserRouter([
     {
@@ -10,12 +11,18 @@ export const router = createBrowserRouter([
         children: [
             {
                 index: true,
-                element: <Products/>
+                element: <Products/>,
+                loader: productsLoader
             },
             {
                 path: "productos/nuevo",
                 element: <NewProduct/>,
                 action: newProductAction
+            },
+            {
+                path: "productos/:id/editar", //RDA PATTERN - RESOURCE-ORIENTED DESIGN
+                element: <EditProduct/>,
+                loader: editProductLoader
             }
         ]
     }
