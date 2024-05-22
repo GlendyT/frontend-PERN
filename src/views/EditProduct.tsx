c19bd555d6f1e85710d05e4dd138e6e7541258e4
GlendyT <glendytuyuc@gmail.com>
2024-05-22 15:15:19 -0600

    agregar las funcionalizades de editar, eliminar, cambiar de disponibilidad con fetcher

--- src/views/EditProduct.tsx
+++ src/views/EditProduct.tsx
@@ -5,20 +5,27 @@ import {
   useActionData,
   redirect,
   LoaderFunctionArgs,
+  useLoaderData,
 } from "react-router-dom";
 import ErrorMessage from "../components/ErrorMessage";
-import { addProduct, getProductById } from "../services/ProductService";
+import {
+  getProductById,
+  updateProduct,
+} from "../services/ProductService";
+import { Product } from "../types";
+import ProductForm from "../components/ProductForm";
 
 export async function loader({ params }: LoaderFunctionArgs) {
   if (params.id !== undefined) {
-   const product = await getProductById(+params.id);
-   console.log(product)
+    const product = await getProductById(+params.id);
+    if (!product) {
+      return redirect("/");
+    }
+    return product;
   }
-
-  return {};
 }
 
-export async function action({ request }: ActionFunctionArgs) {
+export async function action({ request, params }: ActionFunctionArgs) {
   const data = Object.fromEntries(await request.formData());
   let error = "";
   if (Object.values(data).includes("")) {
@@ -28,11 +35,19 @@ export async function action({ request }: ActionFunctionArgs) {
     return error;
   }
 
-  await addProduct(data);
-  return redirect("/");
+  if (params.id !== undefined) {
+    await updateProduct(data, +params.id);
+    return redirect("/");
+  }
 }
 
+const availabilityOptions = [
+  { name: "Disponible", value: true },
+  { name: "No Disponible", value: false },
+];
+
 export default function EditProduct() {
+  const product = useLoaderData() as Product;
   const error = useActionData() as string;
 
   return (
@@ -49,34 +64,29 @@ export default function EditProduct() {
 
       {error && <ErrorMessage>{error}</ErrorMessage>}
       <Form className="mt-10" method="POST">
+
+        <ProductForm product={product}/>
         <div className="mb-4">
-          <label className="text-gray-800" htmlFor="name">
-            Nombre Producto:
+          <label className="text-gray-800" htmlFor="availability">
+            Disponibilidad:
           </label>
-          <input
-            id="name"
-            type="text"
+          <select
+            id="availability"
             className="mt-2 block w-full p-3 bg-gray-50"
-            placeholder="Nombre del Producto"
-            name="name"
-          />
-        </div>
-        <div className="mb-4">
-          <label className="text-gray-800" htmlFor="price">
-            Precio:
-          </label>
-          <input
-            id="price"
-            type="number"
-            className="mt-2 block w-full p-3 bg-gray-50"
-            placeholder="Precio Producto. ej. 200, 300"
-            name="price"
-          />
+            name="availability"
+            defaultValue={product?.availability.toString()}
+          >
+            {availabilityOptions.map((option) => (
+              <option key={option.name} value={option.value.toString()}>
+                {option.name}
+              </option>
+            ))}
+          </select>
         </div>
         <input
           type="submit"
           className="mt-5 w-full bg-pink-800 p-2 text-white font-bold text-lg cursor-pointer rounded"
-          value="Registrar Producto"
+          value="Guardar Cambios"
         />
       </Form>
     </>
